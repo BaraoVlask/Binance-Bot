ef8326737dc8683af20cd8b19853f6848bec0922
Guilherme Billig <guilhermebillig@outlook.com>
2023-02-20 08:02:53 -0300

    ajustment on decimal quantities

---
+++ database/migrations/2023_02_20_105345_alter_buy_price_from_price_ranges_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('price_ranges', function (Blueprint $table) {
+            $table->float('buy_price', 8, 8)
+                ->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('price_ranges', function (Blueprint $table) {
+            $table->float('buy_price')
+                ->unique()
+                ->change();
+        });
+    }
+};
